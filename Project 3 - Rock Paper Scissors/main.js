307e8bb2c25723f7b4a9d2668decf627a77eb366
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-05-13 23:00:17 +0200

    Prepare playGame function

--- Project 3 - Rock Paper Scissors/main.js
+++ Project 3 - Rock Paper Scissors/main.js
@@ -59,6 +59,4 @@ function playRound(computerChoice, playerChoice) {
 	if (playerChoice === computerChoice) console.log("It's a tie!");
 }
 
-function gameJudge() {}
-
-playRound(computerChoice, playerChoice);
+function playGame() {}
