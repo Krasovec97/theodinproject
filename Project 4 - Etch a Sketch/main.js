cd84cd923e2442ea00f908c9f900f24f8f321f27
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-05-30 14:32:09 +0200

    Add fonts and some basic styling

--- Project 4 - Etch a Sketch/main.js
+++ Project 4 - Etch a Sketch/main.js
@@ -1,28 +1,26 @@
 const userInput = 16;
 
 const elements = {
-	gridContainer: document.getElementById('grid-container'),
+	gridContainer: document.getElementById('main-content__grid-container'),
 };
 
 const gridGenerate = (userInput) => {
 	const rows = Array.from(Array(userInput));
 	const cols = Array.from(Array(userInput));
-	const currentCells = 0;
-	const cellsNeeded = rows.length * cols.length;
 
 	elements.gridContainer.style.setProperty('--grid-cols', cols.length);
 	elements.gridContainer.style.setProperty('--grid-rows', rows.length);
 
-	const cellGenerate = (amount, current) => {
-		while (current !== amount) {
-			const cell = document.createElement('div');
-			cell.classList.add('grid-item');
-			elements.gridContainer.appendChild(cell);
-			current++;
-		}
-	};
+	// Generate cells in grid
+	const cellsNeeded = rows.length * cols.length;
+	let currentCells = 0;
 
-	cellGenerate(cellsNeeded, currentCells);
+	while (currentCells !== cellsNeeded) {
+		const cell = document.createElement('div');
+		cell.classList.add('grid-item');
+		elements.gridContainer.appendChild(cell);
+		currentCells++;
+	}
 };
 
 gridGenerate(userInput);
