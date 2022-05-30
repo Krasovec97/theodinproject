9023527f75de491825f74cb4ab9f5449f8579770
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-05-30 15:28:16 +0200

    Add working changable grid size

--- Project 4 - Etch a Sketch/main.js
+++ Project 4 - Etch a Sketch/main.js
@@ -1,18 +1,17 @@
-const userInput = 16;
+let userInput = 16;
 
 const elements = {
 	gridContainer: document.getElementById('main-content__grid-container'),
+	slider: document.getElementById('grid-size-input'),
+	sliderOutput: document.querySelector('.grid-size__output'),
 };
 
 const gridGenerate = (userInput) => {
-	const rows = Array.from(Array(userInput));
-	const cols = Array.from(Array(userInput));
-
-	elements.gridContainer.style.setProperty('--grid-cols', cols.length);
-	elements.gridContainer.style.setProperty('--grid-rows', rows.length);
+	elements.gridContainer.style.setProperty('--grid-cols', userInput);
+	elements.gridContainer.style.setProperty('--grid-rows', userInput);
 
 	// Generate cells in grid
-	const cellsNeeded = rows.length * cols.length;
+	const cellsNeeded = userInput * userInput;
 	let currentCells = 0;
 
 	while (currentCells !== cellsNeeded) {
@@ -21,6 +20,16 @@ const gridGenerate = (userInput) => {
 		elements.gridContainer.appendChild(cell);
 		currentCells++;
 	}
+};
+
+elements.slider.oninput = () => {
+	elements.sliderOutput.textContent = `Desired grid size: ${elements.slider.value} x ${elements.slider.value}`;
+};
+
+elements.slider.onmouseup = () => {
+	userInput = elements.slider.value;
+	elements.gridContainer.innerHTML = '';
+	gridGenerate(userInput);
 };
 
 gridGenerate(userInput);
