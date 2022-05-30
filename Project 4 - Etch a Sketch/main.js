008182cbdd34ffca74cf42e4e5dcaab7673a5c16
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-05-30 14:11:37 +0200

    Add first fully working implementation of grid generation

--- Project 4 - Etch a Sketch/main.js
+++ Project 4 - Etch a Sketch/main.js
@@ -7,18 +7,22 @@ const elements = {
 const gridGenerate = (userInput) => {
 	const rows = Array.from(Array(userInput));
 	const cols = Array.from(Array(userInput));
+	const currentCells = 0;
+	const cellsNeeded = rows.length * cols.length;
 
 	elements.gridContainer.style.setProperty('--grid-cols', cols.length);
 	elements.gridContainer.style.setProperty('--grid-rows', rows.length);
 
-	const cellGenerate = () => {
-		const cell = document.createElement('div');
-		cell.classList.add('grid-item');
-		elements.gridContainer.appendChild(cell);
+	const cellGenerate = (amount, current) => {
+		while (current !== amount) {
+			const cell = document.createElement('div');
+			cell.classList.add('grid-item');
+			elements.gridContainer.appendChild(cell);
+			current++;
+		}
 	};
 
-	cols.forEach(() => cellGenerate());
-	rows.forEach(() => cellGenerate());
+	cellGenerate(cellsNeeded, currentCells);
 };
 
 gridGenerate(userInput);
