8e40ee9ae3a8bbd9089cdcedfbce830ae6e879d5
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-05-31 10:12:34 +0200

    Add reset button and option to color the grid

--- Project 4 - Etch a Sketch/main.js
+++ Project 4 - Etch a Sketch/main.js
@@ -4,6 +4,17 @@ const elements = {
 	gridContainer: document.getElementById('main-content__grid-container'),
 	slider: document.getElementById('grid-size-input'),
 	sliderOutput: document.querySelector('.grid-size__output'),
+	resetBtn: document.querySelector('#reset-button'),
+};
+
+const colorManagement = (color) => {
+	const gridItems = document.querySelectorAll('.grid-item');
+
+	gridItems.forEach((item) => {
+		item.addEventListener('mouseenter', (event) => {
+			event.target.style.setProperty('background-color', `${color}`);
+		});
+	});
 };
 
 const gridGenerate = (userInput) => {
@@ -11,7 +22,7 @@ const gridGenerate = (userInput) => {
 	elements.gridContainer.style.setProperty('--grid-rows', userInput);
 
 	// Generate cells in grid
-	const cellsNeeded = userInput * userInput;
+	const cellsNeeded = Math.pow(userInput, 2);
 	let currentCells = 0;
 
 	while (currentCells !== cellsNeeded) {
@@ -20,16 +31,28 @@ const gridGenerate = (userInput) => {
 		elements.gridContainer.appendChild(cell);
 		currentCells++;
 	}
+
+	colorManagement('red');
 };
 
-elements.slider.oninput = () => {
-	elements.sliderOutput.textContent = `Desired grid size: ${elements.slider.value} x ${elements.slider.value}`;
-};
+const controlsInit = () => {
+	elements.slider.oninput = () => {
+		elements.sliderOutput.textContent = `Desired grid size: ${elements.slider.value} x ${elements.slider.value}`;
+	};
 
-elements.slider.onmouseup = () => {
-	userInput = elements.slider.value;
-	elements.gridContainer.innerHTML = '';
-	gridGenerate(userInput);
+	elements.slider.onmouseup = () => {
+		userInput = elements.slider.value;
+		elements.gridContainer.innerHTML = '';
+		gridGenerate(userInput);
+	};
+
+	elements.resetBtn.addEventListener('click', () => {
+		const cells = document.querySelectorAll('.grid-item');
+		cells.forEach((cell) => {
+			cell.style.setProperty('background-color', 'white');
+		});
+	});
 };
 
 gridGenerate(userInput);
+controlsInit();
