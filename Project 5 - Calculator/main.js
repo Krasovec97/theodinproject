27190ae7affd9e9a2c39278df0811313b9680c63
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-06-08 08:18:05 +0200

    Remove unneccesary functions

--- Project 5 - Calculator/main.js
+++ Project 5 - Calculator/main.js
@@ -1,31 +1,15 @@
-const calcAdd = (num1, num2) => {
-	return num1 + num2;
-};
-
-const calcSubstract = (num1, num2) => {
-	return num1 - num2;
-};
-
-const calcMultiply = (num1, num2) => {
-	return num1 * num2;
-};
-
-const calcDivide = (num1, num2) => {
-	return num1 / num2;
-};
-
 const operate = (operator, num1, num2) => {
 	switch (operator) {
 		case 'add':
-			return calcAdd(num1, num2);
+			return num1 + num2;
 
 		case 'substract':
-			return calcSubstract(num1, num2);
+			return num1 - num2;
 
 		case 'multiply':
-			return calcMultiply(num1, num2);
+			return num1 * num2;
 
 		case 'divide':
-			return calcDivide(num1, num2);
+			return num1 / num2;
 	}
 };
