c549265500599f67b4760c1fe88aa6f8a71de74c
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-06-08 08:01:03 +0200

    Add initial files for project, and some basic functions in main.js

---
+++ Project 5 - Calculator/main.js
@@ -0,0 +1,15 @@
+const calcAdd = (num1, num2) => {
+	return num1 + num2;
+};
+
+const calcSubstract = (num1, num2) => {
+	return num1 - num2;
+};
+
+const calcMultiply = (num1, num2) => {
+	return num1 * num2;
+};
+
+const calcDivide = (num1, num2) => {
+	return num1 / num2;
+};
