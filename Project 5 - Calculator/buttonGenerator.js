59198f480c9717f06c4c5304a650ef3248899fa6
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-06-08 11:07:22 +0200

    Add first numpad generator

--- Project 5 - Calculator/buttonGenerator.js
+++ Project 5 - Calculator/buttonGenerator.js
@@ -4,15 +4,43 @@ const elements = {
 	calculatorOperators: document.querySelector('.calculator-body__operators'),
 };
 
-const generateGrid = () => {
-	let grid = Array.from({ length: 16 }, (value, index) => index);
+const numpadGenerator = () => {
+	let numberGrid = Array.from({ length: 11 }, (value, index) => index);
 
-	grid.forEach((element, index) => {
+	numberGrid.forEach((element, index) => {
 		let button = document.createElement('button');
-		button.textContent = index;
-		button.id = index;
-		elements.calculatorBody.appendChild(button);
+		const buttonManage = setButtons(index, button);
+		button.id = `btn${index}`;
+		button.classList.add('btn');
+		elements.calculatorNumpads.appendChild(button);
 	});
 };
 
-generateGrid();
+const setButtons = (index, button) => {
+	switch (index) {
+		case 0:
+			return (button.textContent = '7');
+		case 1:
+			return (button.textContent = '8');
+		case 2:
+			return (button.textContent = '9');
+		case 3:
+			return (button.textContent = '4');
+		case 4:
+			return (button.textContent = '5');
+		case 5:
+			return (button.textContent = '6');
+		case 6:
+			return (button.textContent = '1');
+		case 7:
+			return (button.textContent = '2');
+		case 8:
+			return (button.textContent = '3');
+		case 9:
+			return (button.textContent = '0');
+		case 10:
+			return (button.textContent = '=');
+	}
+};
+
+numpadGenerator();
