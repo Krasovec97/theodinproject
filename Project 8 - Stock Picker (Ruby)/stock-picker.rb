5baa59da8cb871669742afe1c9de3b4cdfa4c613
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-07-12 12:21:52 +0200

    Started work on Porject 8

---
+++ Project 8 - Stock Picker (Ruby)/stock-picker.rb
@@ -0,0 +1,11 @@
+# Implement a method stock_picker that takes in an array of stock prices,
+# one for each hypothetical day. It should return a pair of days representing
+# the best day to buy and the best day to sell. Days start at 0.
+
+def stock_picker (array)
+    #Buy first
+    
+
+end
+stock_picker([17,3,6,9,15,8,6,1,10])
+
